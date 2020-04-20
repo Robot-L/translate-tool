89bb771d11fc7875a5f9aba021bc953f23d93293
luozhan <luozhan@asiainfo.com>
2020-04-20 13:59:57 +0800

    new

--- src/test/java/com/r/myspring/SampleTest.java
+++ src/test/java/com/r/myspring/SampleTest.java
@@ -2,24 +2,41 @@ package com.r.myspring;
 
 import com.r.myspring.UserMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.executor.BaseExecutor;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SampleTest {
+public class SampleTest implements ApplicationContextAware {
 
     @Resource
     private AccountMapper accountMapper;
+    @Autowired
+    private static ApplicationContext context;
+    @Resource
+    private JdbcTemplate template;
+    @Resource
+    private SqlSessionTemplate template1;
+    @Resource
+    private SqlSessionFactory factory;
 
     @Test
     public void testSelect() {
@@ -30,5 +47,17 @@ public class SampleTest {
         // System.out.println(accountMapper.selectById(4));
         System.out.println(dictValueByCode);
         // userList.forEach(System.out::println);
+        BaseExecutor executor = context.getBean(BaseExecutor.class);
+        try(Connection connection = executor.getTransaction().getConnection()){
+            System.out.println(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        ;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
     }
 }
